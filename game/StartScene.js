f52ea7d6b1474e102665faa1c0b138133ed36466
tplatt77 <tplatt77@users.noreply.github.com>
2024-06-03 00:13:21 -0400

    Simple name changes and adding title to start scene

--- game/StartScene.js
+++ game/StartScene.js
@@ -4,6 +4,7 @@ class StartScene extends Phaser.Scene {
 	}
 
 	create() {
+		this.add.text( 35, 150, 'Pizza Man\'s Last Stand', {fill: '#FF0000', fontSize: '30px'})
 		this.add.text( 150, 250, 'Click to start!', {fill: '#000000', fontSize: '20px'})
 		this.input.on('pointerdown', () => {
 			this.scene.stop('StartScene')
